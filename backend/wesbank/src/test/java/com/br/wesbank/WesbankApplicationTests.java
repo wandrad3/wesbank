df1466da2e3b9bf99c69962ef040929a736eb1f8
wandrad3 <wesleyandrade298@gmail.com>
2024-05-18 11:30:29 -0300

    initiate project

---
+++ backend/wesbank/src/test/java/com/br/wesbank/WesbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.wesbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
